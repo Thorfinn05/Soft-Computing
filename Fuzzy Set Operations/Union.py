74d4dfb8557667dc23f5e643f786fa6941498445
Thorfinn05 <rudranild04@gmail.com>
2026-02-14 19:51:50 +0530

    Add triangular membership function implementation and plotting

---
+++ Fuzzy Set Operations/Union.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def triangular_mf(x, a, b, c):
+    return np.maximum(0, np.minimum((x-a)/(b-a), (c-x)/(c-b)))
+
+x = np.linspace(0, 10, 1000)
+
+setA = triangular_mf(x, 1, 3, 5)
+setB = triangular_mf(x, 2, 6, 8)
+
+union = np.maximum(setA, setB)
+
+plt.plot(x, setA, label=f"setA", linestyle='dashed')
+plt.plot(x, setB, label=f"setB", linestyle='dashed')
+plt.plot(x, union, label=f'Union', linewidth=2)
+plt.fill_between(x, union, color='green', alpha=0.2)
+plt.xlabel('x values')
+plt.ylabel('Membership Degree')
+plt.title('Triangular Membership Function')
+plt.legend()
+plt.grid()
+plt.show()
