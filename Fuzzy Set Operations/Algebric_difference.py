2bdd51d716560c85bcc8612a65b02c37b733c33f
Thorfinn05 <rudranild04@gmail.com>
2026-02-14 20:20:31 +0530

    Add implementation and plotting for algebraic difference of triangular membership functions

---
+++ Fuzzy Set Operations/Algebric_difference.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def triangular_mf(x, a, b, c):
+    return np.maximum(0, np.minimum((x-a)/(b-a), (c-x)/(c-b)))
+
+x = np.linspace(0, 10, 1000)
+
+setA = triangular_mf(x, 1, 3, 5)
+setB = triangular_mf(x, 2, 6, 8)
+
+compB = 1 - setB
+
+alg_diff = np.maximum(0, setA - setB)
+
+plt.plot(x, setA, label=f"setA", linestyle='dashed')
+plt.plot(x, setB, label=f"setB", linestyle='dashed')
+plt.plot(x, alg_diff, label=f'alg_diff', linewidth=2)
+plt.xlabel('x values')
+plt.ylabel('Membership Degree')
+plt.title('Triangular Membership Function')
+plt.legend()
+plt.grid()
+plt.show()
