16fc7c9d69e5ae95544dcf4b270c35a96016582b
Thorfinn05 <rudranild04@gmail.com>
2026-02-23 00:41:43 +0530

    Implement Random Forest model with accuracy evaluation and visualization

---
+++ ML Models/Random-Forest.py
@@ -0,0 +1,24 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.ensemble import RandomForestClassifier
+
+X, y = load_iris(return_X_y=True)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+acc = accuracy_score(y_test, y_pred)
+print(round(acc, 4))
+
+plt.bar(["RF"], [acc])
+plt.show()
+
+sns.heatmap(confusion_matrix(y_test, y_pred), annot=True)
+plt.show()
